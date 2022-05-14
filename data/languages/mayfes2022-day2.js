08477d050026857125ad270aa394d45b414d1cb8
n4o847 <22975590+n4o847@users.noreply.github.com>
2022-05-15 05:23:00 +0900

    mayfes2022-day2: replace languages

--- data/languages/mayfes2022-day2.js
+++ data/languages/mayfes2022-day2.js
@@ -3,35 +3,35 @@ const langsData = require('../langs.json');
 const languages = {
 	// squares
 	0: '',
-	1: 'ppap',
-	2: 'aheui',
-	3: 'cpp-compile-time-clang',
-	4: 'tex',
+	1: 'produire',
+	2: 'cpp-compile-time-clang',
+	3: 'golfscript',
+	4: 'mines',
 	5: '',
 	6: '',
-	7: 'bash-busybox',
-	8: 'compile-time-typescript',
-	9: 'jq',
+	7: 'whitespace',
+	8: 'aheui',
+	9: 'haskell',
 	10: 'ocaml',
 	11: '',
 
 	// hexagons
-	12: 'perl',
-	13: 'befunge98',
-	14: 'fernando',
-	15: 'starry',
-	16: 'produire',
-	17: 'php',
-	18: 'fish',
-	19: 'mao',
+	12: 'csharp',
+	13: 'tetris',
+	14: 'node',
+	15: 'mao',
+	16: 'fish',
+	17: 'wysiscript',
+	18: 'jellyfish',
+	19: 'compile-time-typescript',
 
 	// octagons
-	20: 'whitespace',
-	21: 'node',
+	20: 'bash-busybox',
+	21: 'cpp-clang',
 	22: 'c-gcc',
 	23: 'python3',
 	24: 'ruby',
-	25: 'brainfuck-esotope',
+	25: 'brainfuck-esomer',
 };
 
 module.exports = Array(26)
